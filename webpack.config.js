7c7a539cd904bc29dd9276b34f5a11839fb6e1ca
Alessandro-Miranda <ad.lmiranda2018@gmail.com>
2021-10-27 20:33:57 -0300

    Adicionado o mode e corrigido o path de entrada

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        app: './build/countdownBrowser.js'
+        app: './build/browser.js'
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.join(__dirname, 'dist.browser')
-    }
+        path: path.join(__dirname, 'build.browser')
+    },
+    mode: 'production'
 }
