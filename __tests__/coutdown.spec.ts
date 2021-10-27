6fcee014f05475215e892e616c754109ba9db987
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-10-27 17:07:33 -0300

    Renomeado o arquivo

--- __tests__/coutdown.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, test } from '@jest/globals';
-
-describe('Countdown', () => {
-    test.todo('Should return an error if no one target date has been passed');
-    test.todo('Should return the difference between actual time and targe date');
-    test.todo('Should return 0 if target date is reached');
-})
