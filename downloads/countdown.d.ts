c1c34abdf7cb2e19a7ec92037f2826b1fb4cd415
Alessandro Miranda <ad.lmiranda2018@gmail.com>
2021-11-29 16:00:02 -0300

    pasta contendo os arquivos de download do projeto

---
+++ downloads/countdown.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+import { TimeSplit } from "./types/globals";
+/**
+ * @author Alessandro Miranda
+ * @description Calcula a diferença, em milisegundos, entre a data atual e data alvo
+ * e faz o dispatch do setState, após 1 segundo, para atualizar o componente
+ * @param targetDate Data final
+ * @param dispatchFn Função para atualizar o componente
+*/
+declare function countdown(targetDate: string, dispatchFn: React.Dispatch<React.SetStateAction<TimeSplit>> | ((timeRemaining: TimeSplit) => void)): void;
+export { countdown };
